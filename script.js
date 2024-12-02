a814e3ec9ad4397decb7867a207aae1e1be12152
ANJ-NISH <155297183+ANJ-NISH@users.noreply.github.com>
2024-12-02 11:40:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your code here
+function drag(ev)
+{
+	ev.dataTransfer.setData("text",ev.target.id);
+}
+function allowdrop(ev)
+{
+	ev.preventDefault();
+}
+function drop(ev)
+{
+	ev.preventDefault();
+	let data=ev.dataTransfer.getData("text");
+	let ele1=document.getElementById(data);
+	let ele2=document.getElementById(ev.target.id);
+
+    let img1=ele1.style.backgroundImage;
+	let img2=ele2.style.backgorundImage;
+
+	ele1.style.backgroundImage=img2;
+	ele2.style.backgorundImage=img1;
+}
